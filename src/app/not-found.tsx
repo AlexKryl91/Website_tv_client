80e4a8d8707a888fc629f37683bf41fa6edb9647
Alexander Krylovsky PC <cryls@yandex.ru>
2025-03-12 20:03:56 +0300

    Custom 404 page | title & desc

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,10 @@
 import Page404 from '@/components/Page404/Page404';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '404 Page',
+  description: 'Error 404 - Page not found',
+};
 
 export default function NotFound() {
   return (

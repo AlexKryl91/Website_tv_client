5401bca6ee8f0896204a1594d27f57d9bedcfa59
Alexander Krylovsky PC <cryls@yandex.ru>
2025-03-12 19:56:44 +0300

    Custom 404 page - bug fix

--- src/components/Page404/Page404.tsx
+++ src/components/Page404/Page404.tsx
@@ -1,33 +1,33 @@
-import { TLangSet } from '@/types/types';
+// import { TLangSet } from '@/types/types';
 import classes from './Page404.module.scss';
 import { roboto } from '@utils/fonts';
 
-type TPage404 = {
-  lang: TLangSet;
-};
+// type TPage404 = {
+//   lang: TLangSet;
+// };
 
-const TEXT_CONTENT = {
-  ru: {
-    header: 'Страница ошибки. 404 - страница не найдена',
-    msg: 'Ой! Страница не найдена',
-    text: 'Извините, страница, которую вы ищете, не существует',
-  },
-  en: {
-    header: 'Error page. 404 - Page not found',
-    msg: 'Oops! Page not found',
-    text: "Sorry, the page you're looking for doesn't exist",
-  },
-};
+// const TEXT_CONTENT = {
+//   ru: {
+//     header: 'Страница ошибки. 404 - страница не найдена',
+//     msg: 'Ой! Страница не найдена',
+//     text: 'Извините, страница, которую вы ищете, не существует',
+//   },
+//   en: {
+//     header: 'Error page. 404 - Page not found',
+//     msg: 'Oops! Page not found',
+//     text: "Sorry, the page you're looking for doesn't exist",
+//   },
+// };
 
-const Page404 = ({ lang }: TPage404) => {
-  const { header, msg, text } = TEXT_CONTENT[lang];
-
+const Page404 = () => {
   return (
     <div className={`${classes.container} ${roboto.className}`}>
-      <h1 className="sr-only">{header}</h1>
+      <h1 className="sr-only">Error page. 404 - Page not found</h1>
       <p className={classes.code}>404</p>
-      <h2 className={classes.msg}>{msg}</h2>
-      <p className={classes.text}>{text}</p>
+      <h2 className={classes.msg}>Oops! Page not found</h2>
+      <p className={classes.text}>
+        Sorry, the page you&apos;re looking for doesn&apos;t exist
+      </p>
     </div>
   );
 };
